bdbf43615089f805ab840612270cd75574f51233
ahmad <ahmadrgul@gmail.com>
2025-06-13 14:50:07 +0500

    done with dumb login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route, useLocation } from 'react-router';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -6,11 +6,15 @@ import Doctors from './pages/Doctors';
 import Doctor from './pages/Doctor';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Login from './pages/Login';
 
 const App = () => {
+  const location = useLocation();
+  const isLoginOrRegister = location.pathname === '/login' || location.pathname === '/register'
+
   return (
     <div className='max-w-fit mx-auto px-4 sm:px-8 md:px-20 lg:px-32 xl:px-60'>
-      <Nav />
+      {!isLoginOrRegister && <Nav />} 
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/home' element={<Home />} />
@@ -18,8 +22,9 @@ const App = () => {
         <Route path='/doctors/:id' element={<Doctor />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
-      <Footer />
+      {!isLoginOrRegister && <Footer />}
     </div>
   )
 }
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,24 @@
+const Login = () => {
+  return (
+    <main className="flex items-center justify-center font-outfit h-screen">
+        <div className="w-[570px] border border-[#D4D4D4] py-14 px-12 rounded-2xl">
+            <h1 className="text-3xl text-[#4B5563] font-medium mb-2">Login</h1>
+            <h3 className="text-[#4B5563] text-lg">Please login to book appointment</h3>
+            <form className="my-10 flex flex-col gap-4">
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="email" className="text-[#4B5563] text-xl">Email</label>
+                    <input type="email" id="email" className="border-[#DADADA] border h-14 w-full rounded-lg"/>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="password" className="text-[#4B5563] text-xl">Password</label>
+                    <input type="password" id="password" className="border-[#DADADA] border h-14 w-full rounded-lg"/>
+                </div>
+                <input type="submit" value="Login" className="bg-primary text-gray-100 text-xl font-medium h-14 w-full rounded-lg mt-4" />
+            </form>
+            <p className="text-xl text-[#4B5563]">Don't have an account? <a href="/login" className="underline text-primary">Register Here</a></p>
+        </div>
+    </main>
+  )
+}
+
+export default Login;
