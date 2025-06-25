372eb65780d69b19139f9138ed651d62708b7b6b
ahmad <ahmadrgul@gmail.com>
2025-06-25 06:04:45 +0500

    modified handleAPIError

--- frontend/src/utils/handleAPIError.js
+++ frontend/src/utils/handleAPIError.js
@@ -1,12 +1,22 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
 export const handleAPIError = (error) => {
     if (axios.isAxiosError(error)) {
         if (error.response) {
-            const status = error.response?.status;
-            const errors = error.response?.data?.errors;
+            const status = error.response.status;
+            const errors = error.response.data?.errors;
             
-            errors.forEach(err => {toast.error(`${err.attr ? err.attr : '' + ':'} ${err.detail}`)});
+            errors.forEach(err => {
+                const attr = err.attr ? `${err.attr}: ` : '';
+                toast.error(`${attr}${err.detail}`);
+            });
+        } else if (error.request){
+            toast.error(`Server not responding. Please check your network connection.`);
+        } else {
+            toast.error("Error setting up a request.")
         }
     } else {
-    toast.error("An unexpected error ocurred")
+        toast.error("An unexpected error occurred.");
     }
-}
+};
