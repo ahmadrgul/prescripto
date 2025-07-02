471150981a444d6463a0001398ce98b7d810b679
ahmad <ahmadrgul@gmail.com>
2025-07-02 11:40:15 +0500

    little modifications to remove footer from some pages

--- frontend/src/layouts/UserLayout.jsx
+++ frontend/src/layouts/UserLayout.jsx
@@ -4,14 +4,14 @@ import Footer from "../components/Footer";
 
 const UserLayout = () => {
   const location = useLocation();
-  const isLoginOrRegister =
-    location.pathname === "/login" || location.pathname === "/register";
+  const showNav = location.pathname !== "/login" && location.pathname !== "/register"
+  const showFooter = location.pathname !== "/login" && location.pathname !== "/doctors" && location.pathname !== "/register" && location.pathname !== "/me" && location.pathname !== "/appointments";
 
   return (
-    <div className="max-w-fit mx-auto px-4 sm:px-8 md:px-20 lg:px-32 xl:px-60">
-      {!isLoginOrRegister && <Nav />}
+    <div className="mx-auto px-4 sm:px-8 md:px-20 lg:px-32 xl:px-60">
+      {showNav && <Nav />}
       <Outlet />
-      {!isLoginOrRegister && <Footer />}
+      {showFooter && <Footer />}
     </div>
   );
 };
