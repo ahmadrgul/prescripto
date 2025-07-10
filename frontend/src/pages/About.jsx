b7c74a2c9c0418d71c0c9dda6be91a48aa80398e
ahmad <ahmadrgul@gmail.com>
2025-07-10 10:59:55 +0500

    about page responsive-ui

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <h1 className="text-center text-[#4B5563] font-semibold text-3xl">
         ABOUT <span className="text-[#1F2937]">US</span>
       </h1>
-      <div className="flex gap-10 mt-10 items-center">
+      <div className="flex flex-col lg:flex-row gap-10 mt-10 items-center">
         <div>
           <img src={assets.about_image} />
         </div>
@@ -37,24 +37,24 @@ const About = () => {
       <h1 className="text-[#4B5563] font-semibold text-xl mt-20 mb-10">
         WHY <span className="text-[#1F2937]">CHOOSE US</span>
       </h1>
-      <div className="flex font-outfit border border-[#ABABAB]">
-        <div className="px-20 py-16 w-1/3 border-[#ABABAB] border-r">
+      <div className="flex flex-col md:flex-row font-outfit border border-[#ABABAB]">
+        <div className="px-4 md:px-20 py-8 md:py-16 md:w-1/3 border-b border-[#ABABAB] md:border-r">
           <h3 className="text-[#1F2937] font-semibold">EFFICIENCY:</h3>
-          <p className="text-[#4B5563] leading-6 mt-6">
+          <p className="text-[#4B5563] leading-6 mt-2 md:mt-6">
             Streamlined appointment scheduling that fits into your busy
             lifestyle.
           </p>
         </div>
-        <div className="px-20 py-16 w-1/3 border-[#ABABAB] border-r">
+        <div className="px-4 md:px-20 py-8 md:py-16 md:w-1/3 border-b border-[#ABABAB] md:border-r">
           <h3 className="text-[#1F2937] font-semibold">CONVENIENCE:</h3>
-          <p className="text-[#4B5563] leading-6 mt-6">
+          <p className="text-[#4B5563] leading-6 mt-2 md:mt-6">
             Access to a network of trusted healthcare professionals in your
             area.
           </p>
         </div>
-        <div className="px-20 py-16 w-1/3">
+        <div className="px-4 md:px-20 py-8 md:py-16 md:w-1/3">
           <h3 className="text-[#1F2937] font-semibold">PERSONALIZATION:</h3>
-          <p className="text-[#4B5563] leading-6 mt-6">
+          <p className="text-[#4B5563] leading-6 mt-2 md:mt-6">
             Tailored recommendations and reminders to help you stay on top of
             your health.
           </p>
